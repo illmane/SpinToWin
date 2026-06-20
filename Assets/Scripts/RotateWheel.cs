5853b8c9bcc0dfeb1d641eafec7da8319adf7806
illmane <illmane974@gmail.com>
2026-06-20 10:56:21 +0100

    made physics for the helm

---
+++ Assets/Scripts/RotateWheel.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class RotateWheel : MonoBehaviour
+{
+    public float angularChangeInDegrees;
+    public float maxAngularVelocity;
+    private Rigidbody2D rb;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        InputPlayerRotation();
+    }
+
+    private void InputPlayerRotation()
+    {
+        var impulse = (Input.GetAxis("Horizontal") * Mathf.Deg2Rad) * rb.inertia;
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            if(rb.angularVelocity < -maxAngularVelocity) {
+            rb.angularVelocity = -maxAngularVelocity;
+        }
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            if(rb.angularVelocity > maxAngularVelocity) {
+                rb.angularVelocity = maxAngularVelocity;
+            } 
+        }
+
+        rb.AddTorque(-impulse*angularChangeInDegrees, ForceMode2D.Impulse);
+    }
+}
